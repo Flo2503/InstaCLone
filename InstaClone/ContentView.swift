fbfebff1a4e5d525719cd710cca08e2368436e1f
Flo2503 <bruel.florent@gmail.com>
2020-07-19 12:30:51 +0200

    create navigation bar

--- InstaClone/ContentView.swift
+++ InstaClone/ContentView.swift
@@ -10,12 +10,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        TabView {
+            NavigationView {
+                HomeView()
+                    .navigationBarTitle("Instagram", displayMode: .inline)
+                    .navigationBarItems(leading: Button(action: {}, label: { Image(systemName: "camera")}),
+                                        trailing: Button(action: {}, label: { Image(systemName: "paperplane")}))
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
     }
 }
