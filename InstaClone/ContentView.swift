70587f7d8ba7628c8fc5f3151103f0f28ebd7d25
Flo2503 <bruel.florent@gmail.com>
2020-07-22 23:03:23 +0200

    create main scrollview with srtories circles

--- InstaClone/ContentView.swift
+++ InstaClone/ContentView.swift
@@ -17,6 +17,9 @@ struct ContentView: View {
                     .navigationBarItems(leading: Button(action: {}, label: { Image(systemName: "camera")}),
                                         trailing: Button(action: {}, label: { Image(systemName: "paperplane")}))
             }
+            .tabItem {
+                Image(systemName: "house.fill")
+            }
         }
     }
 }
@@ -29,6 +32,26 @@ struct ContentView_Previews: PreviewProvider {
 
 struct HomeView: View {
     var body: some View {
-        Text("Home")
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        ForEach(Author.all) { author in
+                            VStack {
+                                Image(author.photoName)
+                                .resizable()
+                                .frame(width: 70, height: 70)
+                                .scaledToFit()
+                                .clipShape(Circle())
+                                .overlay(
+                                    Circle().stroke(Color.red, style: StrokeStyle(lineWidth: 2))
+                                .frame(width: 75, height: 77)
+                                )
+                            }
+                        }
+                    } .padding(.all, 10)
+                }
+            }
+        }
     }
 }
